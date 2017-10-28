79c2f6d145dfe33334394a21358fce6a9a9f46db
sidharth-r <na>
2017-10-28 23:04:22 +0530

    added expr tree building added single symbol graph added dot op graph added graph drawing

--- Form1.cs
+++ Form1.cs
@@ -44,6 +44,8 @@ namespace regexFA
             String regex = textIn.Text;
 
             procRegex(regex);
+
+
         }
 
         private void procRegex(String regex)
@@ -53,19 +55,28 @@ namespace regexFA
             {
                 showInvStr();
                 return;
-            }                
-
-            //int nPara = 0;
-            StringBuilder sub = new StringBuilder();
-            bool gotOpr = false, gotOp = false;
-            char c;
-
-            for (int i = regex.Length - 1; i > 0; i--)
-            {
-                c = regex[i];
-
-               
             }
+
+            BTreeNode root = new BTreeNode();//regex.Last());
+            regex = new string(regex.ToCharArray().Reverse().ToArray());
+            root = makeTree(regex,root);
+
+            drawTree(root);
+
+            foreach(Node n in root.graph.nodes)
+            {
+                graphFA.AddNode(n);
+            }
+
+            foreach(Edge e in root.graph.edges)
+            {
+                graphFA.AddEdge(e.Source,e.Target);
+            }
+
+            gViewer.Graph = null;
+            gViewer.CalculateLayout(graphFA);
+            gViewer.Graph = graphFA;
+            gViewer.Refresh();
         }
 
         private String toRPN(String regex)
@@ -100,7 +111,8 @@ namespace regexFA
                 {
                     if(rpnStack.Count > 0)
                     {
-                        while (p < tokenPrecedence(rpnStack.Peek()))
+                        char t = rpnStack.Peek();
+                        while (p < tokenPrecedence(t) && t != '(')
                         {
                             sb.Append(rpnStack.Pop());
                             if (rpnStack.Count == 0)
@@ -125,6 +137,118 @@ namespace regexFA
             return sb.ToString();
         }
 
+        private BTreeNode makeTree(string regex, BTreeNode root)
+        {
+            root.data = regex.First();
+            regex = regex.Substring(1, regex.Length - 1);
+
+            if (regex.Length == 0)
+                return root;
+
+            BTreeNode left = new BTreeNode();
+            BTreeNode right = new BTreeNode();
+
+            StringBuilder stbr = new StringBuilder();//, stbl = new StringBuilder();
+            String strr, strl;
+
+            int n = 1, i = -1;
+            bool gotOpr = false;
+            foreach (char c in regex)
+            {
+                i++;
+
+                if(n == 0)
+                {
+                    break;
+                }
+
+                if(tokenPrecedence(c) == 0)
+                {
+                    gotOpr = true;
+                    stbr.Append(c);
+                    n--;
+                }
+                else if(!gotOpr)
+                {
+                    stbr.Append(c);
+                    if(c != '*')
+                        n++;
+                }
+                else
+                {
+                    showInvStr();
+                    return null;
+                }
+            }
+
+            if(root.data == '*')
+            {
+                strl = stbr.ToString();
+                strr = null;
+            }
+            else
+            {
+                strr = stbr.ToString();
+                strl = regex.Substring(i, regex.Length - i);
+            }
+            
+            if(strl.Length > 1)
+            {
+                left = makeTree(strl, left);
+            }
+            else
+            {
+                left.data = strl.First();
+                
+            }
+            root.left = left;
+
+            if (strr == null)
+            {
+                right = null;
+            }
+            else if(strr.Length > 1)
+            {
+                right = makeTree(strr, right);
+            }
+            else
+            {
+                right.data = strr.First();
+            }
+            root.right = right;
+
+            return root;
+        }
+
+        private void drawTree(BTreeNode root)
+        {
+            char left, right;
+
+            if (root == null)// || tokenPrecedence(root.data) == 0)
+                return;
+            
+            drawTree(root.left);
+            drawTree(root.right);
+
+            char c = root.data;
+            if(c == '*')
+            {
+
+            }
+            else if(c == '+')
+            {
+
+            }
+            else if (c == '.')
+            {
+                root.graph = new GraphDot(root.left.graph, root.right.graph);
+            }
+            else
+            {
+                root.graph = new GraphSymbol(c);
+            }
+        }
+
         private void showInvStr()
         {
             MessageBox.Show("Invalid regex", "Error", MessageBoxButtons.OK);
@@ -142,6 +266,116 @@ namespace regexFA
                 return 1;
             else
                 return 0;
+        }
+    }
+
+    public class BTreeNode
+    {
+        public char data;
+        public BTreeNode left;
+        public BTreeNode right;
+        public GraphBase graph;
+
+        public BTreeNode()
+        {
+            left = null;
+            right = null;
+        }
+
+        public BTreeNode(char d)
+        {
+            data = d;
+            left = null;
+            right = null;
+        }
+
+        void addLeft(BTreeNode n)
+        {
+            left = n;
+        }
+
+        void addRight(BTreeNode n)
+        {
+            right = n;
+        }
+
+        BTreeNode getLeft()
+        {
+            return left;
+        }
+
+        BTreeNode getRight()
+        {
+            return right;
+        }
+    }
+
+    public class GraphBase
+    {
+        public int posX, posY;
+        public float scale;
+
+        public List<Node> nodes;
+        public List<Edge> edges;
+
+        public Node anchorStart, anchorEnd;
+    }
+
+    public class GraphSymbol : GraphBase
+    {
+        public GraphSymbol(char label, int x = 0, int y = 0, float s = 1.0f)
+        {
+            posX = x;
+            posY = y;
+            scale = s;
+
+            nodes = new List<Node>();
+            edges = new List<Edge>();
+            Node n = new Node("gsym_" + label);
+            //n.Label = new Microsoft.Msagl.Drawing.Label(""+label);
+            n.LabelText = ""+label;
+            nodes.Add(n);
+
+            anchorStart = nodes[0];
+            anchorEnd = nodes[0];
+        }
+    }
+
+    public class GraphPlus : GraphBase
+    {
+        public GraphPlus(GraphBase a, GraphBase b, int x = 0, int y = 0, float s = 1.0f)
+        {
+
+        }
+    }
+
+    public class GraphDot : GraphBase
+    {
+        public GraphDot(GraphBase a, GraphBase b, int x = 0, int y = 0, float s = 1.0f)
+        {
+            nodes = new List<Node>();
+            edges = new List<Edge>();
+
+            nodes.AddRange(a.nodes);
+            nodes.AddRange(b.nodes);
+            String aStartA = a.anchorStart.Id,
+                aEndA = a.anchorEnd.Id,
+                aStartB = b.anchorStart.Id,
+                aEndB = b.anchorEnd.Id;
+
+            edges.AddRange(a.edges);
+            edges.AddRange(b.edges);
+            edges.Add(new Edge(aEndA,"",aStartB));
+            anchorStart = a.anchorStart;
+            anchorEnd = b.anchorEnd;
+
+            /*
+            nodes.Add(new Node("gdot_" + a + b + "_a"));
+            nodes.Add(new Node("gdot_" + a + b + "_b"));
+            Edge e = new Edge("gdot_" + a + b + "_a", "", "gdot_" + a + b + "_b");
+            anchorStart = nodes[0];
+            anchorEnd = nodes[1];
+            */
         }
     }
 }
